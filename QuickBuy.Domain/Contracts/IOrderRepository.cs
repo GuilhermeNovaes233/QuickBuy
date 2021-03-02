7f683ce464ccf6bbfe9ea3466c4f5507decb013d
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2021-03-02 17:21:38 -0300

    Adicionando contratos

---
+++ QuickBuy.Domain/Contracts/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using QuickBuy.Domain.Entities;
+
+namespace QuickBuy.Domain.Contracts
+{
+    public interface IOrderRepository : IBaseRepositorio<Order>
+    {
+
+    }
+}
---
+++ QuickBuy.Domain/Contracts/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using QuickBuy.Domain.Entities;
+
+namespace QuickBuy.Domain.Contracts
+{
+    public interface IUserRepository : IBaseRepositorio<Product>
+    {
+        User Get(string email, string password);
+        User Get(string email);
+    }
+}
