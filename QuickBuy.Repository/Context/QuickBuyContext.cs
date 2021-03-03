b30677bc9c3cc85f156578fb41c2c0f992fec3eb
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2021-03-02 21:47:51 -0300

    Criando DbContext e DbSets

---
+++ QuickBuy.Repository/Context/QuickBuyContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Domain.Entities;
+using QuickBuy.Domain.ValueObject;
+
+namespace QuickBuy.Repository.Context
+{
+    public class QuickBuyContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItens { get; set; }
+        public DbSet<PaymentMethod> PaymentMethod { get; set; }
+
+    }
+}
