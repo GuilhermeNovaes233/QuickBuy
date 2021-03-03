e854034261ed50325c3b6b3380a1591409130013
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2021-03-02 22:15:13 -0300

    Adicionando arquivos de configuração, contexto e SQL Lite

---
+++ QuickBuy.Repository/Config/PaymentMethodConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickBuy.Domain.ValueObject;
+using System;
+
+namespace QuickBuy.Repository.Config
+{
+    public class PaymentMethodConfiguration : IEntityTypeConfiguration<PaymentMethod>
+    {
+        public void Configure(EntityTypeBuilder<PaymentMethod> builder)
+        {
+            //builder.HasKey
+        }
+    }
+}
--- QuickBuy.Repository/Context/QuickBuyContext.cs
+++ QuickBuy.Repository/Context/QuickBuyContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using QuickBuy.Domain.Entities;
 using QuickBuy.Domain.ValueObject;
+using QuickBuy.Repository.Config;
 
 namespace QuickBuy.Repository.Context
 {
@@ -12,5 +13,16 @@ namespace QuickBuy.Repository.Context
         public DbSet<OrderItem> OrderItens { get; set; }
         public DbSet<PaymentMethod> PaymentMethod { get; set; }
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderItemConfiguration());
+            modelBuilder.ApplyConfiguration(new PaymentMethodConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
